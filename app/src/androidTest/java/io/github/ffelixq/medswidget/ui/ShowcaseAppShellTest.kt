82327716b91d1d5c12cd5abf4cc8bb9f5e907c38
felix <167162073+ffelixq@users.noreply.github.com>
2026-08-12 11:24:56 +0800

    test: use rule-provided semantics node lookup

--- app/src/androidTest/java/io/github/ffelixq/medswidget/ui/ShowcaseAppShellTest.kt
+++ app/src/androidTest/java/io/github/ffelixq/medswidget/ui/ShowcaseAppShellTest.kt
@@ -6,7 +6,6 @@ import androidx.compose.ui.test.hasScrollAction
 import androidx.compose.ui.test.hasText
 import androidx.compose.ui.test.junit4.v2.createComposeRule
 import androidx.compose.ui.test.onAllNodesWithText
-import androidx.compose.ui.test.onNode
 import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
